43fc4205e44329bb81ef9a8b5a5134da83360456
czw <542191040@qq.com>
2022-10-27 23:09:30 +0800

    init

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.ControllerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,9 +11,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 public class Controller {
 
+    @Autowired
+    private ControllerService controllerService;
+
     @GetMapping("/")
     @ResponseBody
     public String test(){
+        controllerService.asyncTest();
+        System.out.println("哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈");
         return "hello world";
     }
 }
